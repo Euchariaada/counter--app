41f9d7cd1997f157bebfae1de46466168a9a79d0
Adaobi Ezelioha <nwakoreucharia@gmail.com>
2022-06-27 20:04:21 +0100

    simple counter

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+
+import React, {useState} from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <button id="increase-button" onClick={() => setCount(count + 1)}>Click to increament</button>
+            <button id="decrease-button" onClick={() => setCount(count - 1)}>Click to decreament</button>
+            <button id="reset-button" onClick={() => setCount(0)}>Click to reset</button>
+            <p>{count}</p>
+        </div>
+    )
+}
+export default Counter;
